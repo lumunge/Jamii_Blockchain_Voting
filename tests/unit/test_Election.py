8735406c50c8aa88fc75a5876f2567de0c856526
lumunge <lumungep12@gmail.com>
2022-07-18 04:45:28 +0300

    rename

--- tests/unit/test_Election.py
+++
@@ -1,9 +0,0 @@
-""""
-Create Ballot;
-Only owner(creator of contract) can create a ballot
-Amount paid by contract creator is correct
-chair is msg.sender
-ballotsMapping maps to correct value
-candidatesAdded to ballotCandidatesArray
-check for ballot in ballots array
-"""
