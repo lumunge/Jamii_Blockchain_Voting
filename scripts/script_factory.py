b2ec2f3299f19633525e695a2b544045bc9ac609
lumunge <lumungep12@gmail.com>
2022-07-19 16:44:36 +0300

    verify contracts, election costs, test variables

--- scripts/script_factory.py
+++ scripts/script_factory.py
@@ -1,5 +1,5 @@
 from brownie import accounts, JamiiFactory, network, config
-from scripts.utils import election_cost, ballot_cost, get_account
+from scripts.utils import election_cost, get_account
 from web3 import Web3
 
 # testAccounts = [accounts[1], accounts[2], accounts[3]]
@@ -9,16 +9,33 @@ ELECTION_NAME = "TEST_ORG"
 
 
 def deploy_election(_organization_name):
-    account = get_account()
-    new_election = JamiiFactory.deploy(_organization_name, {"from": account, "value": election_cost})
+    account = get_account(0)
+    new_election = JamiiFactory.deploy(
+        _organization_name,
+        {"from": account, "value": election_cost},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
     print(f"Election deployed @ {new_election}")
     return new_election
 
-def create_open_ballot(_ballot_name, _ballot_candidates_addr, _ballot_type):
-    account = get_account()
+
+def create_open_ballot(
+    _ballot_name, _ballot_candidates_addr, _ballot_type, _days, _registration_window
+):
+    account = get_account(0)
     new_election = deploy_election("JAVA")
-    new_ballot = new_election.create_open_ballot(_ballot_name, _ballot_candidates_addr, _ballot_type, {"from": account, "value": ballot_cost})
-    print(f"Ballot Created @ {new_ballot}")
+    new_ballot = new_election.create_open_ballot(
+        _ballot_name,
+        _ballot_candidates_addr,
+        _ballot_type,
+        _days,
+        _registration_window,
+        {"from": account},
+    )
+    new_ballot.wait(1)
+    print(
+        f"Ballot Created @ {new_ballot} for {_days} days, Voter Registration open for {_registration_window} days"
+    )
     return new_ballot
 
 
@@ -58,10 +75,15 @@ def create_open_ballot(_ballot_name, _ballot_candidates_addr, _ballot_type):
 #     withdrawTx.wait(1)
 #     print("Funds sent to owner")
 
-ballot_candidates = ["0xf9d48aC9eC8F207AEF93518B51D2CdA61e596904", "0x6c0A17AEe0a1420583446B77f0c8a55e369Bb07e"]
+ballot_candidates = [
+    "0xf9d48aC9eC8F207AEF93518B51D2CdA61e596904",
+    "0x6c0A17AEe0a1420583446B77f0c8a55e369Bb07e",
+]
+
+
 def main():
-    deploy_election("Kenya")
-    create_open_ballot("Ballot1", ballot_candidates, 0)
+    deploy_election("ORG_1")
+    create_open_ballot("Ballot1", ballot_candidates, 0, 10, 4)
     # getBallotCandidates()
     # vote(testAccounts[0])
     # withdraw()
--- scripts/utils.py
+++ scripts/utils.py
@@ -2,10 +2,14 @@ from brownie import network, accounts, config
 
 LOCAL_BLOCKCHAIN_ENVS = ["development", "ganache-local"]
 
-election_cost = 2000000000000000000 # 2 ETH
-ballot_cost = 1000000000000000000 # 1 ETH
+election_cost = 340000000000000000  # $500
+
 
 def get_account(index=None):
-    if(network.show_active() in LOCAL_BLOCKCHAIN_ENVS):
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVS:
         return accounts[0]
     return accounts.add(config["wallets"]["from_key"])
+
+
+def main():
+    account = get_account()
