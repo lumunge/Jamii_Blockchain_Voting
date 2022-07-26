195f57d108e6816be0f094b3fb8ec18e4bfa8681
lumunge <lumungep12@gmail.com>
2022-07-26 03:44:17 +0300

    version II

---
+++ contracts/_JamiiFactoryV2.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.8;
+
+import "../interfaces/IJamiiFactory.sol";
+import "./JamiiBase.sol";
+
+contract JamiiFactoryV2 {
+    /*
+     *
+     * ## Upgrades Here
+     *
+     */
+}
