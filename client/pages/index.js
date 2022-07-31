cefb859c0faa8325f892292a984657e6f7375e61
lumunge <lumungep12@gmail.com>
2022-07-31 18:02:18 +0300

    home page, navbar, light/dark mode

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,169 @@
+import { useState } from "react";
+// import useMedia from "../utils/useMedia";
 import Head from "next/head";
+import Link from "next/link";
+import useLocalStorage from "use-local-storage";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Box,
+  Button,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+  Divider,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 
-export default function Home() {
+import { nav_items_map } from "../utils/functions";
+
+const useMedia = (theme) => {
+  return theme;
+};
+
+const Home = (props) => {
+  const { window } = props;
+
+  const defaultDark = useMedia("(prefers-color-scheme: dark)").matches;
+
+  const drawerWidth = 240;
+  const nav_items = ["Use Cases", "Success Stories", "Help Center", "Blog"];
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [theme, set_theme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+
+  const switch_theme = () => {
+    const new_theme = theme === "light" ? "dark" : "light";
+    set_theme(new_theme);
+    console.log(new_theme);
+  };
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Jamii Blockchain Voting
+      </Typography>
+      <Divider />
+      <List>
+        {nav_items.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} data-theme={theme}>
       <Head>
         <title>Jamii Blockchain Voting</title>
         <meta name="description" content="A voting system on the blockchain." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            {" "}
+            <MenuIcon />
+          </IconButton>
+          {/* <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Jamii Blockchain Voting
+          </Typography> */}
+          <Box
+            // variant="h6"
+            // component="div"
+            sx={{ flexGrow: 1 }}
+          >
+            {nav_items.map((item) => (
+              <Link href={nav_items_map.get(item)}>
+                <Button
+                  key={item}
+                  sx={{
+                    flexGrow: 1,
+                    color: "#fff",
+                    textTransform: "capitalize",
+                  }}
+                >
+                  {item}
+                </Button>
+              </Link>
+            ))}
+          </Box>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            <Link href="/create_ballot">
+              <Button
+                sx={{
+                  flexGrow: 1,
+                  color: "#fff",
+                  textTransform: "capitalize",
+                }}
+              >
+                Get Started
+              </Button>
+            </Link>
+            {theme === "dark" ? (
+              <Button onClick={switch_theme}>
+                <DarkModeOutlinedIcon sx={{ color: "#000" }} />
+              </Button>
+            ) : (
+              <Button onClick={switch_theme}>
+                <LightModeOutlinedIcon sx={{ color: "#000" }} />
+              </Button>
+            )}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
@@ -96,16 +250,25 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://ethereum.org/en/dapps/"
           target="_blank"
           rel="noopener noreferrer"
+          className={styles.footer_details}
         >
           Powered by{" "}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image
+              src="/ethereum.svg"
+              alt="Ethereum Logo"
+              width={72}
+              height={35}
+            />
           </span>
+          ethereum
         </a>
       </footer>
     </div>
   );
-}
+};
+
+export default Home;
