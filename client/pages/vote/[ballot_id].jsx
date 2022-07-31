8e69f06804b3604e59dd6f534f9bced55bb9845b
lumunge <lumungep12@gmail.com>
2022-07-31 15:41:01 +0300

    voting

--- client/pages/vote/[ballot_id].jsx
+++ client/pages/vote/[ballot_id].jsx
@@ -1,103 +1,190 @@
 import React, { useState, useEffect } from "react";
+import { getWeb3 } from "../../utils/getWeb3";
+import map from "../../../build/deployments/map.json";
 import { useRouter } from "next/router";
-import { get_ballot_candidates, vote } from "../../wrapper/wrapper";
+import {
+  get_ballot_candidates,
+  vote,
+  get_account,
+} from "../../wrapper/wrapper";
 // import { Paper, TextField } from "@mui/material";
+
+import { useDispatch, useSelector } from "react-redux";
+import { wrapper } from "../../store/store";
+import { add_ballot_candidates } from "../../store/ballot_slice";
+import {
+  login,
+  add_connected_account,
+  add_chain_id,
+  add_web_3,
+  add_factory,
+} from "../../store/auth-slice";
 
 const Vote = () => {
   const router = useRouter();
+  const dispatch = useDispatch();
+
   const { ballot_id } = router.query;
-  const [ballot_candidates, set_ballot_candidates] = useState([]);
   const [selected_candidate, set_selected_candidate] = useState("");
 
-  const get_candidates = async (_ballot_id) => {
-    const candidates = await get_ballot_candidates(_ballot_id);
-    set_ballot_candidates(candidates);
-    console.log(candidates);
-    console.log("CHOSEN ONE", selected_candidate);
-  };
+  const [chain_id, set_chain_id] = useState(0);
+  const [web_3, set_web_3] = useState({});
+  const [voted, set_voted] = useState(false);
 
-  const cast_vote = async (e) => {
-    e.preventDefault();
-    await vote(selected_candidate, ballot_id);
-    console.log("voted!!");
-  };
+  // const chain_id = useSelector((state) => state.auth.chain_id);
+  const jamii_factory = useSelector((state) => state.auth.factory);
+  const account = useSelector((state) => state.auth.connected_account);
+
+  const ballot_candidates = useSelector(
+    (state) => state.ballot.ballot_candidates
+  );
 
   const handle_box_change = (e) => {
     set_selected_candidate(e.target.value);
   };
 
+  const init = async () => {
+    const web3 = await getWeb3();
+    set_web_3(web3);
+
+    if (
+      typeof window !== "undefined" &&
+      typeof window.ethereum !== "undefined"
+    ) {
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        const accounts = await web3.eth.getAccounts();
+        const chain_id = parseInt(await web3.eth.getChainId());
+
+        dispatch(login());
+        // dispatch(add_web_3(web3));
+        // dispatch(add_chain_id(chain_id));
+
+        let _chain_id = 0;
+        if (chain_id === 42) {
+          _chain_id = 42;
+        }
+        if (chain_id === 1337) {
+          _chain_id = 1337;
+        }
+        set_chain_id(_chain_id);
+      } catch (error) {
+        if (error.message == "User rejected the request.") {
+          set_error("Connect your Metamask Wallet!");
+          // set_notification(!notification);
+        } else {
+          console.log(error);
+          // set_notification(!notification);
+          // notify(error, "You have to connect your Metamask Wallet!")
+        }
+      }
+    }
+  };
+
+  const load_contract = async (chain, contract_name) => {
+    let address;
+    try {
+      address = map[chain][contract_name][0];
+    } catch (e) {
+      console.log(
+        `Couldn't find any deployed contract "${contract_name}" on the chain "${chain}".`
+      );
+      return undefined;
+    }
+
+    let contract_artifact;
+    try {
+      contract_artifact = await import(
+        `../../../build/deployments/${chain}/${address}.json`
+      );
+      console.log(contract_artifact);
+    } catch (e) {
+      console.log(
+        `Failed to load contract artifact "../../../build/deployments/${chain}/${address}.json"`
+      );
+      return undefined;
+    }
+    console.log("WEB_3# ", web_3);
+    return new web_3.eth.Contract(contract_artifact.abi, address);
+  };
+
+  const load = async () => {
+    const _jamii_factory = await load_contract(chain_id, "JamiiFactory");
+    const ballot = await _jamii_factory?.methods.get_ballot(ballot_id).call();
+    const candidates = await _jamii_factory?.methods
+      .get_candidates(ballot_id)
+      .call();
+    const account = await get_account();
+
+    dispatch(add_connected_account(account));
+    dispatch(add_factory(_jamii_factory));
+    dispatch(add_ballot_candidates(candidates));
+
+    console.log("GAGAGAA: ", _jamii_factory);
+    console.log("BALLOTELLI: ", ballot);
+    console.log("ACCOUNT: ", account);
+    console.log("ChAIN_ID: ", chain_id);
+    console.log("WEB#: ", web_3);
+    console.log("CANDIDATES: : ", candidates);
+    return _jamii_factory;
+  };
+
+  const cast_vote = async (e) => {
+    e.preventDefault();
+    await jamii_factory?.methods
+      .vote(selected_candidate, ballot_id)
+      .send({ from: account, gas: 3000000 })
+      .on("receipt", async () => {
+        // notification
+        set_voted(true);
+        console.log("Voter Successfully Voted!!");
+      });
+  };
+
+  useEffect(() => {
+    init();
+    load();
+  }, []);
+
   return (
     <>
-      <h1>Ballot Id: {ballot_id}</h1>
-      <div>
-        <h4>Candidates</h4>
-        <button onClick={() => get_candidates(ballot_id)}>run</button>
-        <form onSubmit={(e) => cast_vote(e)}>
-          {ballot_candidates && (
+      {!ballot_candidates ? (
+        <>
+          <button onClick={() => load()}>Enter Ballot</button>
+        </>
+      ) : (
+        <>
+          {!voted ? (
+            <form onSubmit={(e) => cast_vote(e)}>
+              {ballot_candidates && (
+                <>
+                  {Object.keys(ballot_candidates)?.map((key) => (
+                    <div key={key}>
+                      <input
+                        type="checkbox"
+                        name="candidate"
+                        value={ballot_candidates[key]}
+                        onChange={handle_box_change}
+                      />
+                      <label for="candidate">
+                        Candidate: {ballot_candidates[key]}
+                      </label>
+                      <br></br>
+                    </div>
+                  ))}
+                </>
+              )}
+              <button type="submit">Vote</button>
+            </form>
+          ) : (
             <>
-              {Object.keys(ballot_candidates)?.map((key) => (
-                <div key={key}>
-                  <input
-                    type="checkbox"
-                    name="candidate"
-                    value={ballot_candidates[key]}
-                    onChange={handle_box_change}
-                  />
-                  <label for="candidate">
-                    Candidate: {ballot_candidates[key]}
-                  </label>
-                  <br></br>
-                  {/* <h4>Address: {ballot_candidates[key]}</h4> */}
-                </div>
-              ))}
+              <h1>You already cast your vote in this</h1>
             </>
           )}
-          <button type="submit">Vote</button>
-        </form>
-      </div>
+        </>
+      )}
     </>
   );
 };
-
-// export const getServerSideProps = async ({ ballot_id }) => {
-//   console.log(ballot_id);
-//   const data = await get_ballot_candidates(ballot_id);
-//   //   const data = { ballot_id: 5 };
-
-//   return {
-//     props: {
-//       data,
-//     },
-//   };
-//   // try-catch removed for simplification
-// };
-
-// export async function getStaticProps() {
-//   const ballot_candidates = await get_ballot_candidates(_ballot_id);
-//   return {
-//     props: {
-//       ballot_candidates,
-//     },
-//     // paths: [`/vote/6dff1b94-8792-45af-8bc5-df9afc3e8b07`],
-//     paths: [{ params: { ballot_id: "ballot_id" } }],
-//     fallback: true,
-//   };
-// }
-
-// export async function getStaticPaths() {
-//   // change your API url to get ALL categories
-//   const res = await fetch(process.env.APIpath + "/api/public/getCategories");
-//   const posts = await res.json();
-
-//   // Get the paths we want to pre-render based on posts, play with params variable you are returning
-//   const paths = posts.map((post) => ({
-//     params: { id: post._id },
-//   }));
-
-//   // We'll pre-render only these paths at build time.
-//   // { fallback: blocking } will server-render pages
-//   // on-demand if the path doesn't exist.
-//   return { paths, fallback: "blocking" };
-// }
 
 export default Vote;
