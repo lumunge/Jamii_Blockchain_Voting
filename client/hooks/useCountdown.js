80661bd032465ab4e4af2627b05e7cd0b9d84924
lumunge <lumungep12@gmail.com>
2022-08-02 14:52:38 +0300

    countdown timer hook

---
+++ client/hooks/useCountdown.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const useCountdown = (target_date) => {
+  const from_date = new Date(target_date).getTime();
+
+  const [count_down, set_count_down] = useState(
+    from_date - new Date().getTime()
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      set_count_down(from_date - new Date().getTime());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [from_date]);
+
+  return return_dates(count_down);
+};
+
+const return_dates = (count_down) => {
+  // calculate time left
+  const days = Math.floor(count_down / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (count_down % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((count_down % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((count_down % (1000 * 60)) / 1000);
+
+  return [days, hours, minutes, seconds];
+};
+
+export { useCountdown };
