848934730c26a41db41ea75ea6d24e6f5da20c32
lumunge <lumungep12@gmail.com>
2022-08-07 22:36:17 +0300

    footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -3,36 +3,25 @@ import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
   return (
-    <>
-      <footer className={styles.footer}>
-        <a
-          href="https://ethereum.org/en/dapps/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.footer_details}
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image
-              src="/ethereum.svg"
-              alt="Ethereum Logo"
-              width={72}
-              height={35}
-            />
-          </span>
-          ethereum
-        </a>
-        <div className={styles.footer_image_container}>
+    <footer className={styles.footer}>
+      <a
+        href="https://ethereum.org/en/dapps/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.footer_details}
+      >
+        Powered by{" "}
+        <span className={styles.logo}>
           <Image
-            className={styles.footer_image}
-            src="/ballot_img1.png"
+            src="/ethereum.svg"
             alt="Ethereum Logo"
-            width={300}
-            height={50}
+            width={72}
+            height={35}
           />
-        </div>
-      </footer>
-    </>
+        </span>
+        ethereum
+      </a>
+    </footer>
   );
 };
 
