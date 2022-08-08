64045785113aed0efcf9629645eeb81433ea6222
lumunge <lumungep12@gmail.com>
2022-08-08 19:43:02 +0300

    right sidebar mobile

--- client/components/RightSideBar.jsx
+++ client/components/RightSideBar.jsx
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 
 import { add_show_form, add_active_tab } from "../store/ballot_slice";
+import { add_theme } from "../store/theme_slice";
 
 import { Grid, Box, Button, Typography, Paper } from "@mui/material";
 
@@ -8,6 +9,10 @@ import { Grid, Box, Button, Typography, Paper } from "@mui/material";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import PrintOutlinedIcon from "@mui/icons-material/PrintOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import LanguageIcon from "@mui/icons-material/Language";
+import VerifiedOutlinedIcon from "@mui/icons-material/VerifiedOutlined";
 
 // Components
 import Notification from "../components/Notification";
@@ -17,6 +22,7 @@ import styles from "../styles/create_ballot.module.css";
 
 const RightSideBar = () => {
   const dispatch = useDispatch();
+  const theme = useSelector((state) => state.theme.current_theme);
   const connected = useSelector((state) => state.auth.is_connected);
   const ballots = useSelector((state) => state.ballot.ballots);
 
@@ -31,15 +37,39 @@ const RightSideBar = () => {
     dispatch(add_show_form(true));
   };
 
+  const switch_theme = () => {
+    const new_theme = theme === "light" ? "dark" : "light";
+    dispatch(add_theme(new_theme));
+  };
+
   return (
     <>
       <Grid
+        container
         sx={{
           height: "100%",
           padding: "1rem 2rem",
           flexDirection: "column",
         }}
       >
+        <Grid item sx={{ display: "flex" }}>
+          {theme === "light" ? (
+            <Button onClick={switch_theme}>
+              <DarkModeOutlinedIcon sx={{ color: "#000" }} />
+            </Button>
+          ) : (
+            <Button onClick={switch_theme}>
+              <LightModeOutlinedIcon sx={{ color: "#000" }} />
+            </Button>
+          )}
+          <Button>
+            {theme === "dark" ? (
+              <LanguageIcon sx={{ color: "#000" }} />
+            ) : (
+              <LanguageIcon sx={{ color: "#000" }} />
+            )}
+          </Button>
+        </Grid>
         <Button
           variant="outlined"
           color="success"
@@ -56,19 +86,23 @@ const RightSideBar = () => {
           variant="outlined"
           color="success"
           className={styles.right_btns}
+          disabled
         >
           Preview{" "}
-          <VisibilityOutlinedIcon
-            sx={{ position: "relative", right: "-10px" }}
+          <VerifiedOutlinedIcon
+            sx={{ position: "relative", right: "-20px", color: "#FEC600" }}
           />
         </Button>
         <Button
           variant="outlined"
           color="success"
           className={styles.right_btns}
+          disabled
         >
           Print{" "}
-          <PrintOutlinedIcon sx={{ position: "relative", right: "-10px" }} />
+          <VerifiedOutlinedIcon
+            sx={{ position: "relative", right: "-30px", color: "#FEC600" }}
+          />
         </Button>
         <div>
           {!connected && (
