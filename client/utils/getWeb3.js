b9329a36613663a0dec5692db66c5d9606048ea7
lumunge <lumungep12@gmail.com>
2022-08-08 22:55:36 +0300

    deployed to kovan testnet

--- client/utils/getWeb3.js
+++ client/utils/getWeb3.js
@@ -5,7 +5,9 @@ export const getWeb3 = async () => {
   const ethereum = await getEthereum();
   let web3;
 
-  const provider = new Web3.providers.HttpProvider("http://127.0.0.1:8545");
+  const provider = new Web3.providers.HttpProvider(
+    process.env.KOVAN_INFURA_URL
+  );
   web3 = new Web3(provider);
 
   // if (ethereum) {
