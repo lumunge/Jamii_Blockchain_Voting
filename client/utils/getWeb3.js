42030e32b67f1ba2670a158dedae3cba19eb4078
lumunge <lumungep12@gmail.com>
2022-08-26 13:09:20 +0300

    process env infura urls

--- client/utils/getWeb3.js
+++ client/utils/getWeb3.js
@@ -17,9 +17,15 @@ export const getWeb3 = async () => {
   } else {
     // const provider = new Web3.providers.HttpProvider("http://127.0.0.1:8545");
     const provider = new Web3.providers.HttpProvider(
-      process.env.GOERLI_INFURA_URL
+      "http://127.0.0.1:8545" ||
+        process.env.GOERLI_INFURA_URL ||
+        process.env.RINKEBY_INFURA_URL ||
+        process.env.KOVAN_INFURA_URL
     );
+    console.log("I AM PROVIDER ", provider);
     web3 = new Web3(provider);
+
+    console.log("I AM WEB 3 ", web3);
   }
 
   return web3;
